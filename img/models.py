03dc4477e80820440178f105e642e8ca66bbc1f0
mgpo-98 <csi4613@naver.com>
2023-03-22 19:25:53 +0900

    0322

--- img/models.py
+++ img/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+import os
+
+class Document(models.Model):
+    attached = models.FileField('첨부 파일', upload_to='img/zzal')
+
+    def get_filename(self):
+        return os.path.basename(self.attached.name)
--- img/views.py
+++ img/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
+from .forms import DocumentForm
 # Create your views here.
 def index(request):
 
     return render(request, 'img/index.html')
+
+class DocumentCreateView(FormView):
+    template_name = "img/index.html"
+    form_class = DocumentForm
+    success_url = reverse_lazy('document_list')
+
+    def form_valid(self, form):
+        if self.request.FILES:
+            form.instance.attached = self.request.FILES['zzal']
+        form.save()
+        return super().form_valid(form)
