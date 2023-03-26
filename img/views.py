6c3b2acf90bdf9bd5b7137719ffe8b2f82ebb872
mgpo-98 <csi4613@naver.com>
2023-03-26 22:09:44 +0900

    0326

--- img/views.py
+++ img/views.py
@@ -17,3 +17,25 @@ class DocumentCreateView(FormView):
             form.instance.attached = self.request.FILES['zzal']
         form.save()
         return super().form_valid(form)
+    
+def create(request, item_id):
+    item_title = request.GET.get('item_title')
+    print(request.GET.get('item_title'))
+    if request.method == 'POST':
+        review_form = ReviewForm(request.POST, request.FILES)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            # 로그인한 유저 => 작성자네!
+            review.user = request.user 
+            review.item_id = item_id
+            review.item_title = item_title
+            review.save()
+            messages.success(request, '리뷰 작성이 완료되었습니다.')
+            return redirect('reviews:index')
+    else: 
+        review_form = ReviewForm()
+    context = {
+        'review_form': review_form,
+        'item_title': request.GET.get('item_title')
+    }
+    return render(request, 'img/form.html', context=context)
