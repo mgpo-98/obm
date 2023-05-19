25224be8108320fffb203f88e78289e4727f0d9b
mgpo-98 <csi4613@naver.com>
2023-05-19 17:39:39 +0900

    0519

--- img/views.py
+++ img/views.py
@@ -1,10 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
-from .forms import DocumentForm
+from img.forms import DocumentForm
 import random
 
 # Create your views here.
 def index(request):
+  if request.method == 'POST':
+        review_form = DocumentForm(request.POST, request.FILES)
+        if review_form.is_valid():
+            review = review_form.save(commit=False)
+            # 로그인한 유저 => 작성자네!
+            review.save()
+            return redirect('reviews:index')
+  else: 
+        review_form = DocumentForm()
+  context = {
+      'review_form': review_form,  
+    }
+  return render(request, 'img/index.html', context=context)
+  
 
-    return render(request, "img/index.html")
+
