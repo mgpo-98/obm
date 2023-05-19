0e3ce7e32848e1acfe172545b5c6664be9e36644
mgpo-98 <csi4613@naver.com>
2023-05-19 17:30:40 +0900

    0519

--- img/views.py
+++ img/views.py
@@ -3,10 +3,8 @@ from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from .forms import DocumentForm
 import random
+
 # Create your views here.
 def index(request):
-   
-    
-    return render(request, 'img/index.html')
 
-
+    return render(request, "img/index.html")
