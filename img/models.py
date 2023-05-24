972a19f5dfc5e8fbefa315e69f2033e87e986bd9
mgpo-98 <csi4613@naver.com>
2023-05-24 18:13:20 +0900

    0524

--- img/models.py
+++ img/models.py
@@ -1,10 +1,14 @@
 from django.db import models
+from imagekit.processors import ResizeToFill
 
 # Create your models here.
 import os
 
 class Document(models.Model):
-    attached = models.FileField('첨부 파일', upload_to='img/zzal')
+    attached = models.FileField('첨부 파일', upload_to='img/zzal', blank=True,
+                            processors=[ResizeToFill(1200, 960)],
+                            format='JPEG',
+                            options={'quality': 80})
 
     def get_filename(self):
         return os.path.basename(self.attached.name)
