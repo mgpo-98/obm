95cdf144cda721f23133a8c000dc932da85d4cf0
mgpo-98 <csi4613@naver.com>
2023-06-20 22:19:46 +0900

    0620

--- img/views.py
+++ img/views.py
@@ -2,10 +2,12 @@ from django.shortcuts import render, redirect
 from django.views.generic.edit import FormView
 from django.urls import reverse_lazy
 from img.forms import ReviewForm
-import random
+from .models import Review
+
 
 # Create your views here.
 def index(request):
+  image = Review.objects.all()
   if request.method == 'POST':
         review_form = ReviewForm(request.POST, request.FILES)
         if review_form.is_valid():
@@ -15,6 +17,7 @@ def index(request):
         review_form = ReviewForm()
   context = {
       'review_form': review_form,  
+      'image' : image,
     }
   return render(request, 'img/index.html', context=context)
   
