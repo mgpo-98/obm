fa9fed03c9987e26a019284fe3df8ca84513570e
mgpo <60389344+mgpo-98@users.noreply.github.com>
2023-12-10 22:55:54 +0900

    1210

--- obm/views.py
+++ obm/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from img.models import SearchHistory
-
+from django.http import JsonResponse
+from django.db.models import Count
 def main(request):
     pass
     return render(request,'main.html')
 
 def get_realtime_search_rank(request):
     # 최근 검색 기록을 가져옴 (가장 최근에 검색된 키워드 순으로 정렬)
-    recent_searches = SearchHistory.objects.order_by('-search_time')[:5]
-
+    search_history = (
+        SearchHistory.objects
+        .values('query')
+        .annotate(search_count=Count('query'))
+        .order_by('-search_count')[:10])
+ 
     # 가져온 검색 기록에서 검색어만 추출
-    search_rank = [search.query for search in recent_searches]
+    rank_data = [{'query': item['query'], 'search_count': item['search_count']} for item in search_history]
+    
+    return JsonResponse({'rank_data': rank_data})
 
-    return JsonResponse({'rank': search_rank})
