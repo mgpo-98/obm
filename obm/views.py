54dd6dbeb8d7c181612c14a9e45282a036736f6b
mgpo <60389344+mgpo-98@users.noreply.github.com>
2023-12-12 16:33:31 +0900

    1212

--- obm/views.py
+++ obm/views.py
@@ -3,23 +3,16 @@ from img.models import SearchHistory
 from django.http import JsonResponse
 from django.db.models import Count
 
-def get_realtime_search_rank(request):
+def get_popular_search_rank(request):
     search_history = (
         SearchHistory.objects
         .values('query')
         .annotate(search_count=Count('query'))
         .order_by('-search_count')[:10])
-  
+
     rank_data = [{'query': item['query'], 'search_count': item['search_count']} for item in search_history]
-   
+
     return JsonResponse({'rank': rank_data})
 
 def main(request):
-    query = request.GET.get('search')
-    search_history = (
-        SearchHistory.objects
-        .annotate(search_count=Count('query'))
-        .order_by('-search_count')[:10])
-
-    
-    return render(request, 'main.html', {'search_history': search_history})
+    return render(request, 'main.html')
